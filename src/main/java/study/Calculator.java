3d1bb796ec80a8aafb0fa69f4d96850c966fba74
Jsim6342 <snowdrop6342@gmail.com>
2021-10-24 10:49:59 +0900

    계산기 리팩토링

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -4,61 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
-    private String[] operationTypes = {"+", "-", "*", "/"};
+    private static final Pattern regExp = Pattern.compile("^[0-9]*$");
+    private Operator operator;
+    private Formula formula;
 
-    public int StringCalculator(String input) throws Exception {
+    public Calculator(String input) {
+        this.formula = new Formula(input);
+    }
 
-        ArrayList<String> values = new ArrayList<>(Arrays.asList(input.split(" ")));
-        ArrayList<String> operations = new ArrayList<>();
+    public int getResult() {
+        int result = 0;
+        Operator nowOperator = Operator.PLUS;
 
-        int result = Integer.parseInt(values.remove(0));
-
-        if(values.isEmpty()) {
-            return result;
-        }
-
-        for (String value : values) {
-
-            if(isOperation(value)) {
-                operations.add(value);
+        for (String now: formula.getFormula()) {
+            if(regExp.matcher(now).find()) {
+                result = nowOperator.operate(result, Integer.parseInt(now));
                 continue;
             }
-
-            int valueNum = Integer.parseInt(value);
-            result = OperationCalculator(result, operations.remove(0), valueNum);
-
+            nowOperator = Operator.findOperator(now);
         }
+
         return result;
     }
 
-    public int OperationCalculator(int num1, String operation, int num2) throws Exception{
 
-            if(operation.equals("+")){
-                return num1 + num2;
-            }
-            if(operation.equals("-")){
-                return num1 - num2;
-            }
-            if(operation.equals("*")){
-                return num1 * num2;
-            }
-            if(operation.equals("/")){
-                return num1 / num2;
-            }
-
-        throw new Exception("잘못 입력하셨습니다.");
-
-    }
-
-    private boolean isOperation(String value) {
-        for (String operationType : operationTypes) {
-           if(operationType.equals(value)) return true;
-        }
-        return false;
-    }
 
 
 }
